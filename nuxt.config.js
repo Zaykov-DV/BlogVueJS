9f0f8d678a4b38f4298a39d2b31b9883a6974643
Дмитрий <grunger.ekb@gmail.com>
2021-01-11 13:53:24 +0500

    SEO + deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,20 @@
+import pkg from './package'
+import axios from 'axios'
+
 export default {
-  // Global page headers (https://go.nuxtjs.dev/config-head)
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
   head: {
-    title: 'blog',
+    title: pkg.name,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      // { name: 'yandex-verification', content: 'KEY' },
+      // { name: 'google-site-verification', content: 'KEY' },
+      { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
@@ -12,28 +22,68 @@ export default {
     ]
   },
 
-  // Global CSS (https://go.nuxtjs.dev/config-css)
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
   css: [
     { src: '~/assets/scss/main.scss', lang: 'scss' }
   ],
 
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  /*
+  ** Plugins to load before mounting the App
+  */
   plugins: [
     '~plugins/app-components.js'
   ],
 
-  // Auto import components (https://go.nuxtjs.dev/config-components)
-  components: true,
-
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-  ],
-
-  // Modules (https://go.nuxtjs.dev/config-modules)
+  /*
+  ** Nuxt.js modules
+  */
   modules: [
+    // ['@nuxtjs/google-analytics', {
+    //   id: 'UA-key'
+    // }],
+    // ['@nuxtjs/yandex-metrika', {
+    //   id: 'key',
+    //   clickMap: true,
+    //   trackLinks: true
+    // }]
   ],
 
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  /*
+  ** Build configuration
+  */
   build: {
+    /*
+    ** You can extend webpack config here
+    */
+    vendor: [
+      'vue',
+      'axios'
+    ],
+    extend(config, ctx) {
+    }
+  },
+  // generate config
+  generate: {
+    routes() {
+      return axios.get('https://blog-nuxt-95e46-default-rtdb.firebaseio.com/posts.json')
+        .then(res => {
+          // Get ID
+          const postsArray = []
+          for (let key in res.data) {
+            postsArray.push({...res.data[key], id: key})
+          }
+          // Routes
+        return postsArray.map(post => {
+          return '/blog/' + post.id
+        })
+      })
+    }
   }
 }
