ee47378ea1cddfb86ffaa86b642f1f5027b3d645
Дмитрий <grunger.ekb@gmail.com>
2021-01-11 17:09:08 +0500

    localStorage

--- store/index.js
+++ store/index.js
@@ -43,11 +43,21 @@ export const actions = {
       password: authData.password,
       returnSecureToken: true
     })
-      .then((res) => { commit('setToken', res.data.idToken) } )
+      .then((res) => {
+        let token = res.data.idToken
+        commit('setToken', token)
+        localStorage.setItem('token', token)
+      })
       .catch((e) => console.log(e))
+  },
+  initAuth ({commit}) {
+    let token = localStorage.getItem('token')
+    if (!token) return false
+    commit('setToken', token)
   },
   logoutUser ({commit}) {
     commit('destroyToken')
+    localStorage.removeItem('token')
   },
   addPost ({commit}, post) {
     // console.log(post)
